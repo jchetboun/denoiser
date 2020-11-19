e1029a7731030b43eca295c8b79ea8f1e1112d71
Jonathan Chetboun <jchetboun@lightricks.com>
2020-11-19 12:51:46 -0500

    Training without resample.

--- launch_dns_light.sh
+++ launch_dns_light.sh
@@ -12,7 +12,7 @@ python train.py \
   demucs.hidden=32 \
   demucs.depth=4 \
   demucs.kernel_size=4 \
-  demucs.resample=4 \
+  demucs.resample=1 \
   batch_size=128 \
   revecho=1 \
   segment=10 \
